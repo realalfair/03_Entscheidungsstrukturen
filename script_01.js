330986522fcdf985e8e6c6640f4123857f5b6723
realalfair <irina.merzdorf@gmail.com>
2023-07-14 10:31:02 +0200

    a1df

--- script_01.js
+++ script_01.js
@@ -3,3 +3,19 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+const ageJohn = 30;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");

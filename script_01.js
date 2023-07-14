803c8c871a39db2d7aeea2fac14ea1132d5dd537
realalfair <irina.merzdorf@gmail.com>
2023-07-14 10:53:48 +0200

    02 IF

--- script_01.js
+++ script_01.js
@@ -4,7 +4,7 @@
 
 // Deklaration + Assignment
 const ageJohn = 30;
-const ageMark = 30;
+const ageMark = 40;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
@@ -19,3 +19,14 @@ console.log("ageMark: " + ageMark);
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
 console.log("------------------");
+
+/************ IF ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+if(ageJohn > ageMark)
+{
+    console.log("John ist älter.");
+}
+
